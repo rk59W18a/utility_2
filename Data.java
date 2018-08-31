3e3418467bbaa0f11295d63b025c1b835d3fc500
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-31 18:38:40 +0200

    Add files via upload

--- Data.java
+++ Data.java
@@ -5,46 +5,57 @@ import java.time.format.DateTimeFormatter;
 
 public class Data {
 
+	//foreign method, should be in LocalDate
 	public static boolean confrontaDate(LocalDate d1, LocalDate d2) {
 		return d1.isEqual(d2);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static boolean verificaDataSuccessiva(LocalDate d) {
 		return LocalDate.now().isAfter(d);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static boolean verificaDataPrecedente(LocalDate d) {
 		return LocalDate.now().isBefore(d);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static boolean verificaDataCoincidente(LocalDate d) {
 		return LocalDate.now().equals(d);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static LocalDate aumentaNumeroAnni(LocalDate d, int numeroAnni) {
 		return d.plusYears(numeroAnni);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static LocalDate aumentaNumeroGiorni(LocalDate d, int numeroGiorni) {
 		return d.plusDays(numeroGiorni);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static LocalDate diminuisciNumeroGiorni(LocalDate d, int numeroGiorni) {
 		return d.minusDays(numeroGiorni);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static LocalDate getDataAttuale() {
 		return LocalDate.now();
 	}
 	
+	//foreign method, should be in LocalDate
 	public static LocalDate getDataImpostata(int anno, int mese, int giorno) {
 		return LocalDate.of(anno, mese, giorno);
 	}
 	
+	//foreign method, should be in LocalDate
 	public static int getNumeroAnniDataAttuale() {
 		return LocalDate.now().getYear();
 	}
 	
+	//foreign method, should be in LocalDate
 	public static String getDataFormattata(LocalDate d) {
 	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Costanti.FORMATO_DATA);
 	    return d.format(formatter);
